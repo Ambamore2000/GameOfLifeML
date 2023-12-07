9ad41e6ba8d506177829f5d970eb74eaf782757e
Andrew Kim <andkim123@gmail.com>
2023-12-07 01:11:06 -0800

    updated model generation and training

--- gameoflifeml/config.py
+++ gameoflifeml/config.py
@@ -1,32 +1,8 @@
 # config.py for the gameoflifeml project
 
-import os
 
-# MODEL_TYPE: A string variable to define the type of model configuration to be used.
-# It can be set externally via an environment variable. If not set, it defaults to 'optimal'.
-# Possible values: 'low_end', 'optimal', 'high_end'.
-MODEL_TYPE = os.getenv('MODEL_TYPE', 'optimal')
-
-# MODEL_CONFIGS: A dictionary that maps each MODEL_TYPE to its specific configuration.
-# Each configuration is a dictionary with keys 'SIZE', 'STEPS', and 'NUM_SAMPLES'.
-# - 'low_end': Configuration for low-end model, less resource-intensive
-# - 'optimal': Balanced configuration, intended as a default setting
-# - 'high_end': Configuration for high-end model, more resource-intensive
 MODEL_CONFIGS = {
     'optimal': {'SIZE': 100, 'STEPS': 2, 'NUM_SAMPLES': 10000}
 }
 
-# current_config: Retrieves the specific configuration for the current MODEL_TYPE.
-# Uses the .get() method for safe access to the MODEL_CONFIGS dictionary.
-# If MODEL_TYPE is not a key in MODEL_CONFIGS, defaults to the 'optimal' configuration.
-current_config = MODEL_CONFIGS.get(MODEL_TYPE, MODEL_CONFIGS['optimal'])
-
-# SIZE, STEPS, NUM_SAMPLES: Extracted specific settings from the current configuration.
-# These settings can be imported and used across various modules in the project.
-SIZE = current_config['SIZE']
-STEPS = current_config['STEPS']
-NUM_SAMPLES = current_config['NUM_SAMPLES']
-
-# othERS
-PATTERN_RATIO = 0.5
 DATA_PATH = "gameoflifeml/data/"
--- gameoflifeml/train/trainer.py
+++ gameoflifeml/train/trainer.py
@@ -1,23 +1,19 @@
-import numpy as np
-import tensorflow as tf
+import os
+from gameoflifeml.generate.gol import generate_random_data
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, Add
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import Precision, Recall, AUC
 from tensorflow.keras import backend as K
-from gameoflifeml.generate.gol import place_pattern   # Adjust the import path as needed
 from .. import config
 
-# Define the Dice coefficient for use as a metric
 def dice_coefficient(y_true, y_pred, smooth=1e-6):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
-# Define the model architecture
 def residual_block(x, filters, kernel_size=3):
-    """A residual block for the CNN"""
     y = Conv2D(filters, kernel_size, padding='same')(x)
     y = BatchNormalization()(y)
     y = Activation('relu')(y)
@@ -27,72 +23,86 @@ def residual_block(x, filters, kernel_size=3):
     out = Activation('relu')(out)
     return out
 
-def game_of_life_model(input_size=(100, 100, 1)):
-    inputs = Input(input_size)
+def game_of_life_model(size):
+    inputs = Input((size, size, 1))
     x = Conv2D(64, 3, activation='relu', padding='same')(inputs)
 
-    # Add residual blocks
     for _ in range(4):
         x = residual_block(x, 64)
 
     output = Conv2D(1, 1, activation='sigmoid')(x)
     model = Model(inputs, output)
-    # Compile the model with additional metrics
+
     model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', 
                   metrics=['accuracy', Precision(), Recall(), AUC(), dice_coefficient])
     return model
 
-# Data loading function
-def load_data(data_path):
-    data = np.load(data_path)
+def load_data(data):
     input_frames = data[:, 0, :, :].reshape(-1, 100, 100, 1)
     target_frames = data[:, 1, :, :].reshape(-1, 100, 100, 1)
     return input_frames, target_frames
 
-# Create a random glider pattern for testing
-def create_random_glider(size):
-    glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
-    grid = np.zeros((size, size), dtype=int)
-    x, y = np.random.randint(0, size - 3, size=2)
-    place_pattern(grid, glider, (x, y))
-    return grid
-
-# Main execution
-if __name__ == "__main__":
-    # Path to your training data
-    data_path = config.DATA_PATH + "RANDOM_optimal.npy" # Adjust this path
-
-    # Load data
-    input_frames, target_frames = load_data(data_path)
-    print(f"Input Frames Shape: {input_frames.shape}")
-    print(f"Target Frames Shape: {target_frames.shape}")
-
-    # Initialize and train the model
-    model = game_of_life_model()
-    model.fit(input_frames, target_frames, batch_size=16, epochs=3, validation_split=0.2)
-
-    # Evaluate the model
+def evaluate_gol_model(model_name, model, input_frames, target_frames):
     evaluation = model.evaluate(input_frames, target_frames)
-    print("Evaluation Metrics:")
-    print("Loss:", evaluation[0])
-    print("Accuracy:", evaluation[1])
-    print("Precision:", evaluation[2])
-    print("Recall:", evaluation[3])
-    print("AUC:", evaluation[4])
-    print("Dice Coefficient:", evaluation[5])
 
-    # Calculate F1-Score manually
     precision = evaluation[2]
     recall = evaluation[3]
     f1_score = 2 * (precision * recall) / (precision + recall + 1e-8)
-    print("F1-Score:", f1_score)
-    # Save the model
-    model.save(config.DATA_PATH + 'gameoflife_cnn_model.h5')
 
-    # Test the model with a random glider pattern
-    test_grid = create_random_glider(100)
-    predicted_frame = model.predict(np.expand_dims(test_grid, axis=0))
-    predicted_frame_binary = (predicted_frame > 0.5).astype(int)
-    print("Predicted frame:")
-    for row in predicted_frame_binary[0, :, :, 0]:
-        print(' '.join(map(str, row)))
+    evaluation_string = (
+        "Evaluation Metrics:\n"
+        f"Loss: {evaluation[0]}\n"
+        f"Accuracy: {evaluation[1]}\n"
+        f"Precision: {evaluation[2]}\n"
+        f"Recall: {evaluation[3]}\n"
+        f"AUC: {evaluation[4]}\n"
+        f"Dice Coefficient: {evaluation[5]}\n"
+        f"F1-Score: {f1_score}\n"
+    )
+
+    metrics_file_path = os.path.join(config.DATA_PATH, f'{model_name}_metrics.txt')
+
+    with open(metrics_file_path, 'w') as file:
+        file.write(evaluation_string)
+
+def train_gol_model(size=100, steps=2, num_samples=10000, batch_size=16, epochs=3, validation_split=0.2):
+    _model_name = f'gol_{num_samples}_{steps}_{size}_{size}_model'
+
+    _generated_random_gol_data = generate_random_data(size, steps, num_samples)
+    _input_frames, _target_frames = load_data(_generated_random_gol_data)
+
+    _model = game_of_life_model(size)
+    _model.fit(_input_frames, _target_frames, batch_size, epochs, validation_split)
+    evaluate_gol_model(_model_name, _model, _input_frames, _target_frames)
+    _model.save(config.DATA_PATH + f'{_model_name}.h5')    
+
+def train_preset_gol_model(preset):
+    train_gol_model(preset['SIZE'], 
+                    preset['STEPS'], 
+                    preset['NUM_SAMPLES'], 
+                    preset['BATCH_SIZE'], 
+                    preset['EPOCHS'], 
+                    preset['VALIDATION_SPLIT'])
+
+if __name__ == "__main__":
+    MODEL_CONFIGS = {
+        'optimal': {
+            'SIZE': 100,
+            'STEPS': 2,
+            'NUM_SAMPLES': 10000,
+            'BATCH_SIZE': 16,
+            'EPOCHS': 2,
+            'VALIDATION_SPLIT': 0.2
+        },
+        'low-end-test': {
+            'SIZE': 100,
+            'STEPS': 2,
+            'NUM_SAMPLES': 1000,
+            'BATCH_SIZE': 16,
+            'EPOCHS': 5,
+            'VALIDATION_SPLIT': 0.2
+        }
+
+    }
+
+    train_preset_gol_model(MODEL_CONFIGS['optimal'])
